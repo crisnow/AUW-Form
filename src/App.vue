2992ac6f9c03633eefa17d836b5df79a6b3634da
kristi <snowswirlin@gmail.com>
2021-03-30 15:34:50 -1000

    a form

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/auw_211_logo.png">
+    <h1> AUW 211 Data (Report) Request Form</h1>
+    <contact-form />
+  </div>
+</template>
+
+<script>
+import ContactForm from './components/ContactForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
